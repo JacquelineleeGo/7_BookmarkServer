54f822588bbef6d8b27e7f95714174b0259b6862
Jing Li <jacqueline.lee126@gmail.com>
2018-10-15 11:43:14 -0700

    Write do_POST
    do_POST sends a 303 redirect to the form after
    saving a newly submitted URI
    If it fetching the long URI failed, send a 404 error with a useful message.

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -96,7 +96,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 # Delete the following line -- raise NotImplementedError("Step 2 isn't written yet.")
                 self.send_response(303)
                 self.send_header('Location', memory[name])
-            git     self.end_headers()
+                self.end_headers()
             else:
                 # We don't know that name! Send a 404 error.
                 self.send_response(404)
@@ -122,9 +122,11 @@ class Shortener(http.server.BaseHTTPRequestHandler):
         # Check that the user submitted the form fields.
         if "longuri" not in params or "shortname" not in params:
             # 3. Serve a 400 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 3 isn't written yet!")
-
+            #    Delete the following line -- raise NotImplementedError("Step 3 isn't written yet!")
+            self.send_response(404)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write("Please provide a longuri or a shortname or both")
         longuri = params["longuri"][0]
         shortname = params["shortname"][0]
 
@@ -133,14 +135,19 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             memory[shortname] = longuri
 
             # 4. Serve a redirect to the root page (the form).
-            #    Delete the following line.
-            raise NotImplementedError("Step 4 isn't written yet!")
+            # Delete the following line -- raise NotImplementedError("Step 4 isn't written yet!")
+            self.send_response(303)
+            self.send_header('Location', '')
+            self.end_headers()
         else:
             # Didn't successfully fetch the long URI.
-
             # 5. Send a 404 error with a useful message.
-            #    Delete the following line.
-            raise NotImplementedError("Step 5 isn't written yet!")
+            # Delete the following line -- raise NotImplementedError("Step 5 isn't written yet!")
+            self.send_response('404')
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile('Fetching the long URI failed')
+
 
 if __name__ == '__main__':
     server_address = ('', 8000)
